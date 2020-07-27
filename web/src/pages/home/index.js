8089bdd4641440e30913301c5011d5452ef7481f
Schuansk Lucas Tenorio Torres <schuansk@hotmail.com.br>
2020-07-26 22:32:44 -0300

    correcao na versao do history

--- web/src/pages/home/index.js
+++ web/src/pages/home/index.js
@@ -1,13 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+
+import api from '../../services/api'
 
 import { Context } from '../../Context/AuthContext'
 
 export default function Home() {
     const { handleLogout } = useContext(Context)
+    const [ name, setName ] = useState('')
+
+    useEffect(() => {
+        api.get('/user').then((response) => {
+            setName(response.data.userId)
+        })
+    }, []);
 
     return (
         <>
-            <h1>Bem-vindo a Home</h1>
+            <h1>Bem-vindo, { name }</h1>
             <hr />
             <button 
                 type="button"
--- web/src/pages/login/index.js
+++ web/src/pages/login/index.js
@@ -1,18 +1,19 @@
 import React, { useState, useContext } from 'react'
 
+import history from '../../history'
+
 import { Context } from '../../Context/AuthContext'
 
 export default function Login() {
-    const { authenticated, handleLogin } = useContext(Context)
+    const { handleLogin } = useContext(Context)
 
     const [ email, setEmail ] = useState('')
     const [ password, setPassword ] = useState('')
 
-    async function handleSubmit(e) {
+    function handleSubmit(e) {
         e.preventDefault() 
 
         handleLogin(email, password)
-        console.debug('Login', authenticated)
     }
 
     return (
